56514cb0a9aa84e3b39f3f49b5079683524a69de
전성모 <jeonsm22@daum.net>
2024-10-29 22:44:38 +0900

    chore : install sass, redux toolkit, nivo and set redux, redux-persist

--- FrontEnd/src/app/layout.tsx
+++ FrontEnd/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import ClientLayout from '@/redux/ClientLayout';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -12,7 +13,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <ClientLayout>{children}</ClientLayout>
+      </body>
     </html>
   );
 }
