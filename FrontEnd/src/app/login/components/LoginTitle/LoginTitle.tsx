d6dd6aef1ab54c78edf480bbc430432ae9167536
전성모 <jeonsm22@daum.net>
2024-10-30 21:21:23 +0900

    feat: 로그인 페이지 추가

---
+++ FrontEnd/src/app/login/components/LoginTitle/LoginTitle.tsx
@@ -0,0 +1,10 @@
+import styles from './LoginTitle.module.scss';
+
+export default function LoginTitle() {
+  return (
+    <div className={`flex flex-col`}>
+      <span className={styles.title}>MEDICAL COPILOT</span>
+      <span className={`${styles.madeBy} flex justify-end`}>made by Newmes</span>
+    </div>
+  );
+}
