5985c69cc1eaa125362ae192157b51347bd92c57
kwonja <kownja87@gmail.com>
2024-10-30 22:25:13 +0900

    feat : 탭 컴포넌트 구현

--- FrontEnd/src/app/page.tsx
+++ FrontEnd/src/app/page.tsx
@@ -1,10 +1,10 @@
-import Link from 'next/link';
+import Header from '@/components/Header/Header';
 
 export default function Home() {
   return (
     <>
       <div className="">첫 페이지입니다!</div>
-      <Link href="/login">123123</Link>
+      <Header />
     </>
   );
 }
