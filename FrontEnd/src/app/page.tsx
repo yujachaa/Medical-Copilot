f0f4d710ba0d232bae46ac3a391c928012786545
전성모 <jeonsm22@daum.net>
2024-10-30 15:32:17 +0900

    feat : sideBar 컴포넌트 추가 및 react-icons 설치

--- FrontEnd/src/app/page.tsx
+++ FrontEnd/src/app/page.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <>
-      <div className=" ">첫 페이지입니다!</div>
-      <Link href="/login">123</Link>
+      <div className="">첫 페이지입니다!</div>
+      <Link href="/login">123123</Link>
     </>
   );
 }
