04ace3789f07cf435871f6ba691e2e6ffbfc9fd9
JOON-KR <yyj4147@gmail.com>
2024-10-30 13:52:55 +0900

    feat: add security to cloud

---
+++ BackEnd/onpremise/src/main/java/com/newmes/onpremise/domains/quota/domain/Quota.java
@@ -0,0 +1,4 @@
+package com.newmes.onpremise.domains.quota.domain;
+
+public class Quota {
+}
---
+++ BackEnd/onpremise/src/main/java/com/newmes/onpremise/global/security/dto/TokenResponseDto.java
@@ -0,0 +1,4 @@
+package com.newmes.onpremise.global.security.dto;
+
+public record TokenResponseDto(String accessToken, String refreshToken) {
+}
