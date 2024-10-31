a329d722d0dafa3985965a178d2bee61dcd767b8
kwonja <kownja87@gmail.com>
2024-10-31 22:57:49 +0900

    feat : 탭리스트 스토리북 구현

--- FrontEnd/src/app/page.tsx
+++ FrontEnd/src/app/page.tsx
@@ -1,10 +1,7 @@
-import TabBoard from '@/components/Tabs/TabBoard';
-
 export default function Home() {
   return (
     <>
       <div className="">첫 페이지입니다!</div>
-      <TabBoard />
     </>
   );
 }
