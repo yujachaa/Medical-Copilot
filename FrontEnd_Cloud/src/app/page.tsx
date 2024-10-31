83cd02579bf83bcda9de6099e472b45a2e5e5cd0
전성모 <jeonsm22@daum.net>
2024-10-31 15:56:47 +0900

    feat: 관리자 로그인 페이지 추가

--- FrontEnd_Cloud/src/app/page.tsx
+++ FrontEnd_Cloud/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   console.log('123');
   return (
     <div className=" ">
-      <Link href="/">관리자페이지입니다.</Link>
+      <Link href="/login">로그인 페이지로 이동</Link>
     </div>
   );
 }
