06b220f22161e208d765bff0ebc0136155e1d65a
kwonja <kownja87@gmail.com>
2024-10-31 18:58:38 +0900

    feat : 탭 기능 구현

--- FrontEnd/src/components/Tabs/Tab.tsx
+++ FrontEnd/src/components/Tabs/Tab.tsx
@@ -2,24 +2,33 @@ import React from 'react';
 import styles from './Tab.module.scss';
 import { CgClose } from 'react-icons/cg';
 import SelectTabIcons from './components/SelectTabIcons';
+import { tab } from '@/redux/features/tab/tabSlice';
 
 export type PluginType = 'default' | 'cxr' | 'capsule';
 
 type TabProps = {
-  onClose: () => void;
-  text: string;
-  LogoType: PluginType;
+  HandleDelete: (id: number) => void;
+  onClick: () => void;
+  tab: tab;
   isActive: boolean;
 };
 
-export default function Tab({ onClose, text, LogoType = 'default', isActive }: TabProps) {
+export default function Tab({ HandleDelete, onClick, tab, isActive }: TabProps) {
+  const { id, type = 'default', title } = tab;
+  const handleDelete = (e: React.MouseEvent<SVGElement>) => {
+    e.stopPropagation();
+    HandleDelete(id);
+  };
   return (
-    <div className={` ${styles.tab} ${isActive === true && styles.active}`}>
-      {SelectTabIcons(LogoType)}
-      <p>{text}</p>
+    <div
+      className={` ${styles.tab} ${isActive === true && styles.active}`}
+      onClick={onClick}
+    >
+      {SelectTabIcons(type)}
+      <p>{title}</p>
       <CgClose
-        className="w-5 h-5 ml-auto mr-4 cursor-pointer text-rgb0.5"
-        onClick={onClose}
+        className="w-5 h-5 ml-auto cursor-pointer text-rgb0.5"
+        onClick={handleDelete}
       />
     </div>
   );
