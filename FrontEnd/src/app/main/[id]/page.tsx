b92733d514a01f55c7ccee0f2ae70c99dab2ca12
kwonja <kownja87@gmail.com>
2024-11-02 21:53:17 +0900

    feat : 쿼리 검색 바 구현

--- FrontEnd/src/app/main/[id]/page.tsx
+++ FrontEnd/src/app/main/[id]/page.tsx
@@ -1,11 +1,26 @@
 'use client';
 import React from 'react';
-
+import styles from './page.module.scss';
+import Input from '../components/Input';
 type paramsType = {
   params: {
     id: string;
   };
 };
 export default function page({ params }: paramsType) {
-  return <div>{params.id}</div>;
+  console.log(params.id);
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>MEDICAL COPILOT</div>
+      <div className="flex justify-center items-center text-[20px] text-2c2c2c mt-4">
+        Dr. Bell will tell you anything about medical knowledge!
+      </div>
+      <div className="flex justify-center items-center text-[16px] text-2c2c2c">
+        Please type in the questions you`re curious about.
+      </div>
+      <div className="flex justify-center items-center mt-12">
+        <Input />
+      </div>
+    </div>
+  );
 }
--- FrontEnd/tailwind.config.ts
+++ FrontEnd/tailwind.config.ts
@@ -9,11 +9,17 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      rotate: {
+        '135': '135deg',
+      },
       colors: {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
         white: '#ffffff',
         'rgb0.5': '#00000080',
+        '2c2c2c': '#2C2C2C',
+        clip: '#0D99FF',
+        'blue-logo': '#2380EC',
       },
     },
   },
