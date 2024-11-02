3613ce959890f72f50413f3ac9a723efa720ac43
kwonja <kownja87@gmail.com>
2024-11-02 14:52:09 +0900

    fix : sidebar image width : 100% set

---
+++ FrontEnd/src/app/main/layout.tsx
@@ -0,0 +1,12 @@
+import SideBar from '@/components/SideBar/SideBar';
+import TabBoard from '@/components/Tabs/TabBoard';
+import React from 'react';
+
+export default function layout() {
+  return (
+    <>
+      <SideBar />
+      <TabBoard />
+    </>
+  );
+}
