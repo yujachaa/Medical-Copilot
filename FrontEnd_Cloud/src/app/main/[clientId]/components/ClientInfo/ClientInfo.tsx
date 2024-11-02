90afba7367b8205990554513646a922a2ad478ea
전성모 <jeonsm22@daum.net>
2024-11-02 17:56:08 +0900

    feat: 고객사 상세 페이지 반응형 추가

--- FrontEnd_Cloud/src/app/main/[clientId]/components/ClientInfo/ClientInfo.tsx
+++ FrontEnd_Cloud/src/app/main/[clientId]/components/ClientInfo/ClientInfo.tsx
@@ -4,11 +4,13 @@ import styles from './ClientInfo.module.scss';
 export default function ClientInfo({ data }: { data: client }) {
   return (
     <div className={`${styles.main} flex flex-col h-[35%] min-h-[220px] max-h-[260px] pt-4`}>
-      <div className={`flex gap-6 items-end`}>
+      <div className={`${styles.title} flex gap-6 items-end`}>
         <span className={`${styles.name}`}>{data.comName}</span>
         <span className={`${styles.tokens} mr-10`}>50 tokens/week</span>
-        <span className={`${styles.edit} flex justify-center items-center`}>EDIT</span>
-        <span className={`${styles.stop} flex justify-center items-center`}>STOP</span>
+        <div className={`${styles.titleBtns} flex gap-6`}>
+          <span className={`${styles.edit} flex justify-center items-center`}>EDIT</span>
+          <span className={`${styles.stop} flex justify-center items-center`}>STOP</span>
+        </div>
       </div>
       <span className={`${styles.key} pl-1 mt-2`}>Serial key : {data.key}</span>
       <p className={`${styles.usage} mt-3 pl-1`}>
