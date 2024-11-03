9ef9be9a98002018cabd45d9758342e78ca156cf
kwonja <kownja87@gmail.com>
2024-11-03 15:58:51 +0900

    feat : 검색바,플러그인 선택 구현

--- FrontEnd/src/components/Tabs/components/SelectTabIcons.tsx
+++ FrontEnd/src/components/Tabs/components/SelectTabIcons.tsx
@@ -3,8 +3,14 @@ import Default from '../../../assets/images/logo.svg';
 import Cxr from '../../../assets/images/cxr.svg';
 import Capsule from '../../../assets/images/capsule.svg';
 import { PluginType } from '../Tab';
-export default function SelectTabIcons(logoType: PluginType) {
-  if (logoType === 'default') return <Default className="w-8 h-8" />;
-  if (logoType === 'capsule') return <Capsule className="w-8 h-8" />;
-  if (logoType === 'cxr') return <Cxr className="w-8 h-8" />;
+
+type Props ={
+  logoType: PluginType;
+  w? : string;
+  h? : string;
+}
+export default function SelectTabIcons({logoType,w='8',h='8'} : Props) {
+  if (logoType === 'default') return <Default className={`w-${w} h-${h}`} />;
+  if (logoType === 'capsule') return <Capsule className={`w-${w} h-${h}`} />;
+  if (logoType === 'cxr') return <Cxr className={`w-${w} h-${h}`} />;
 }
