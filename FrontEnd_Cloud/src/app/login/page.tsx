74349f6c87efe5edc18689851382da9c1ad0644e
전성모 <jeonsm22@daum.net>
2024-11-04 09:48:52 +0900

    fix: tailwindcss

--- FrontEnd_Cloud/src/app/login/page.tsx
+++ FrontEnd_Cloud/src/app/login/page.tsx
@@ -5,7 +5,7 @@ import styles from './page.module.scss';
 export default function Login() {
   return (
     <div
-      className={`${styles.main} w-screen h-screen overflow-x-hidden flex flex-col items-center pt-[150] gap-10`}
+      className={`${styles.main} w-screen h-screen overflow-x-hidden flex flex-col items-center pt-[150px] gap-10`}
     >
       <LoginTitle />
       <LoginInput />
