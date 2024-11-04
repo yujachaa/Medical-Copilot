98bf46c65ae46be1b8f0ee9778bc47db423f41c1
kwonja <kownja87@gmail.com>
2024-11-04 21:56:12 +0900

    fix : 진료기록 아이콘 수정

--- FrontEnd/src/app/main/components/Input.tsx
+++ FrontEnd/src/app/main/components/Input.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import { HiPaperClip } from 'react-icons/hi2';
 import styles from './Input.module.scss';
 import Send from '@/assets/images/send.svg';
-
+import { FaDatabase } from 'react-icons/fa6';
 export default function Input() {
   return (
     <div className={styles.container}>
-      <label htmlFor="file" className={styles.file}>
-        <HiPaperClip className="rotate-135 w-6 h-6 text-clip" />
+      <label
+        htmlFor="file"
+        className={styles.file}
+      >
+        <FaDatabase className="w-5 h-5 text-clip" />
       </label>
-      <input id="file" type="file" className='hidden w-0 h-0'/>
+      <input
+        id="file"
+        type="file"
+        className="hidden w-0 h-0"
+      />
       <input
         className={styles.input}
         type="text"
