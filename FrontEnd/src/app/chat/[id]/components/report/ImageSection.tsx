3ee8c29f4bcc38ba307c707f56ca1bbfa6375f41
이윤주 <yunj333@gmail.com>
2024-11-04 17:17:42 +0900

    feat: 사진편집 모달 추가

--- FrontEnd/src/app/chat/[id]/components/report/ImageSection.tsx
+++ FrontEnd/src/app/chat/[id]/components/report/ImageSection.tsx
@@ -1,8 +1,18 @@
+'use client';
+
 import Image from 'next/image';
+import { useState } from 'react';
 import styles from './ReportData.module.scss';
 import XrayImg from '@/assets/images/xrayImg.jpg';
+import { MdOutlineDraw } from 'react-icons/md';
+import EditModal from './EditModal';
 
 export default function ImageSection() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
   return (
     <div className={`${styles.info}`}>
       <div>Image</div>
@@ -14,6 +24,14 @@ export default function ImageSection() {
           height={250}
         />
       </div>
+      <div
+        className={styles.editorBtn}
+        onClick={openModal}
+      >
+        <MdOutlineDraw />
+      </div>
+
+      {isModalOpen && <EditModal onClose={closeModal} />}
     </div>
   );
 }
