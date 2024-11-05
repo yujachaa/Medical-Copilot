dc31239ebc519b7a227449b1621d938d97e18d47
JOON-KR <yyj4147@gmail.com>
2024-11-05 16:51:26 +0900

    fix: login

--- BackEnd/onpremise/src/main/java/com/newmes/onpremise/domains/member/controller/MemberController.java
+++ BackEnd/onpremise/src/main/java/com/newmes/onpremise/domains/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.newmes.onpremise.domains.member.controller;
 import com.newmes.onpremise.domains.member.domain.Token;
 import com.newmes.onpremise.domains.member.dto.request.LoginRequestDto;
 import com.newmes.onpremise.domains.member.dto.request.MemberRequestDto;
+import com.newmes.onpremise.domains.member.dto.response.LoginResponseDto;
 import com.newmes.onpremise.domains.member.dto.response.MemberResponseDto;
 import com.newmes.onpremise.domains.member.service.MemberService;
 import com.newmes.onpremise.global.util.HttpResponseUtil;
@@ -29,8 +30,8 @@ public class MemberController {
 
     @PostMapping("/login")
     public ResponseEntity<Map<String, Object>> login(@RequestBody LoginRequestDto requestDto) {
-        var token = memberService.login(requestDto);
-        return httpResponseUtil.createSuccessResponse("success", token);
+        LoginResponseDto response = memberService.login(requestDto);
+        return httpResponseUtil.createSuccessResponse("success", response);
     }
 
     @PostMapping("/logout")
