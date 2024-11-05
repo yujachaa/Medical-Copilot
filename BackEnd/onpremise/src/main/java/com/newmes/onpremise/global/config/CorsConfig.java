cf5d87c15534bc505e9ec2b0c4a85a85c9301f6a
JOON-KR <yyj4147@gmail.com>
2024-11-05 13:34:45 +0900

    fix: cors setting

--- BackEnd/onpremise/src/main/java/com/newmes/onpremise/global/config/CorsConfig.java
+++ BackEnd/onpremise/src/main/java/com/newmes/onpremise/global/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://k11s205.p.ssafy.io", "http:localhost:8080","http:localhost:8081")
+                .allowedOrigins("http://localhost:3000", "https://k11s205.p.ssafy.io", "http:localhost:8080","http:localhost:3001")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
