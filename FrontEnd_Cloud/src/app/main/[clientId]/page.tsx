ed7d65a741f4c9bb720e187f8f5327fd314b24e5
전성모 <jeonsm22@daum.net>
2024-11-07 16:00:47 +0900

    feat: Client API 추가

--- FrontEnd_Cloud/src/app/main/[clientId]/page.tsx
+++ FrontEnd_Cloud/src/app/main/[clientId]/page.tsx
@@ -1,24 +1,14 @@
-import { client } from '@/types/client';
+import { clientDetail } from '@/types/client';
 import ClientBtn from './components/ClientBtn/ClientBtn';
 import ClientGraph from './components/ClientGraph/ClientGraph';
 import ClientInfo from './components/ClientInfo/ClientInfo';
 import styles from './page.module.scss';
-// import { fetchClientDetail } from '@/apis/fetchClientDetail';
+import { fetchClientDetail } from '@/apis/fetchClientDetail';
 // import LogModal from './components/LogModal/LogModal';
 
-const data: client = {
-  id: 1,
-  comName: 'Newmes111',
-  grade: 'DEFAULT',
-  key: '2a197cf4-de8a-4fb4-b768-70bba6c9b3a4',
-  cxrCount: 0,
-  capsuleCount: 0,
-  medGuruCount: 0,
-  totalCount: 0,
-};
-
-export default async function ClientDetailPage() {
-  // const data = await fetchClientDetail(params.clientId);
+export default async function ClientDetailPage({ params }: { params: { clientId: string } }) {
+  const data: clientDetail = await fetchClientDetail(params.clientId);
+  console.log(params.clientId);
   return (
     <div className={`${styles.main} w-screen flex`}>
       <div className={`w-screen flex flex-col p-6`}>
--- FrontEnd_Cloud/src/app/main/components/ClientListItem/ClientListItem.tsx
+++ FrontEnd_Cloud/src/app/main/components/ClientListItem/ClientListItem.tsx
@@ -1,14 +1,13 @@
 import Link from 'next/link';
 import styles from './ClientListItem.module.scss';
 import { FaChevronRight } from 'react-icons/fa';
+import { client } from '@/types/client';
 
-type dataType = { id: number; comName: string; grade: string; key: string };
-
-export default function ClientListItem({ client }: { client: dataType }) {
+export default function ClientListItem({ client }: { client: client }) {
   return (
     <Link
       className={`${styles.main} w-full h-[110px] rounded-[10px] flex pl-4 pt-3 pr-2 pb-3`}
-      href={`/main/${client.id}`}
+      href={`/main/${client.key}`}
     >
       <div className={`w-[95%] h-full flex flex-col`}>
         <span className={`${styles.clientId}`}>
@@ -16,16 +15,15 @@ export default function ClientListItem({ client }: { client: dataType }) {
         </span>
         <span className={`${styles.clientName}`}>{client.comName}</span>
         <div className={`${styles.tag} flex gap-6 h-[35px] justify-between`}>
-          <span className={`${styles.usage}`}>1234</span>
-          <span className={`${styles.week}`}>10weeks</span>
+          <span className={`${styles.usage}`}>{client.totalCount}</span>
+          <span className={`${styles.week}`}>{client.subscription}weeks</span>
           <span
             style={
-              'Error' !== 'Error' ? { backgroundColor: '#1F9DFF' } : { backgroundColor: '#FF8090' }
+              client.availability ? { backgroundColor: '#1F9DFF' } : { backgroundColor: '#FF8090' }
             }
             className={`${styles.state}`}
           >
-            {/* {client.state} */}
-            Error
+            {client.availability ? 'Stable' : 'Pause'}
           </span>
         </div>
       </div>
