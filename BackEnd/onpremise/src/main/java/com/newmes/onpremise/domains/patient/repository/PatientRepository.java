3061cb3ae210e87e0e9b0621d71828b0be52c4f4
JOON-KR <yyj4148@naver.com>
2024-11-08 17:37:47 +0900

    feat:add read patirent agent-image

--- BackEnd/onpremise/src/main/java/com/newmes/onpremise/domains/patient/repository/PatientRepository.java
+++ BackEnd/onpremise/src/main/java/com/newmes/onpremise/domains/patient/repository/PatientRepository.java
@@ -1,7 +1,11 @@
 package com.newmes.onpremise.domains.patient.repository;
 
+import com.newmes.onpremise.domains.patient.domain.Modality;
 import com.newmes.onpremise.domains.patient.entity.PatientEntity;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+import java.util.Optional;
+
 public interface PatientRepository extends ElasticsearchRepository<PatientEntity, String> {
+    Optional<PatientEntity> findFirstByPIDAndModalityOrderByVisitDateDesc(String pid, Modality modality);
 }
