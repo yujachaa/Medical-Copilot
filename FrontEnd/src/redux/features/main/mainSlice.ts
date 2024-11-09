df44ebe58d52f5e8b0170aaea885b541bf06d206
kwonja <kownja87@gmail.com>
2024-11-09 13:51:36 +0900

    feat : 환자선택, 메세지 입력 후 AI 분석 요청 로직 구현

--- FrontEnd/src/redux/features/main/mainSlice.ts
+++ FrontEnd/src/redux/features/main/mainSlice.ts
@@ -8,12 +8,40 @@ export type Patient = {
   modality: string;
 };
 
-const initialState: Patient = {
-  sex: '',
-  age: 0,
-  visitDate: '',
-  pid: '',
-  modality: '',
+export type PatientReqeust = {
+  PID: string;
+  image: string;
+  shootingDate: string;
+  sex: 'FEMALE' | 'MALE' | '';
+  age: number;
+  comments: string;
+  key: string;
+  agent: string;
+};
+
+type initialProps = {
+  patient: Patient;
+  patientRequest: PatientReqeust;
+};
+
+const initialState: initialProps = {
+  patient: {
+    sex: '',
+    age: 0,
+    visitDate: '',
+    pid: '',
+    modality: '',
+  }, // 일반 key와 image는 default로 가지고 있겠습니다
+  patientRequest: {
+    PID: '',
+    image: 'https://newmes.s3.ap-northeast-2.amazonaws.com/classification/test.jpg',
+    shootingDate: '',
+    sex: '',
+    age: 0,
+    comments: '',
+    key: 'ccf97220-30b3-4780-acab-295301698be0',
+    agent: '',
+  },
 };
 
 const mainSlices = createSlice({
@@ -21,14 +49,23 @@ const mainSlices = createSlice({
   initialState: initialState,
   reducers: {
     setPatient: (state, action: PayloadAction<Patient>) => {
-      state.sex = action.payload.sex;
-      state.age = action.payload.age;
-      state.modality = action.payload.modality;
-      state.visitDate = action.payload.visitDate;
-      state.pid = action.payload.pid;
+      state.patient.sex = action.payload.sex;
+      state.patient.age = action.payload.age;
+      state.patient.modality = action.payload.modality;
+      state.patient.visitDate = action.payload.visitDate;
+      state.patient.pid = action.payload.pid;
+
+      state.patientRequest.sex = action.payload.sex;
+      state.patientRequest.age = action.payload.age;
+      state.patientRequest.PID = action.payload.pid;
+      state.patientRequest.shootingDate = action.payload.visitDate;
     },
     setModality: (state, action: PayloadAction<string>) => {
-      state.modality = action.payload;
+      state.patientRequest.agent = action.payload;
+    },
+    //보낼 이미지를 저장하는 함수
+    setReqeustImage: (state, action: PayloadAction<string>) => {
+      state.patientRequest.image = action.payload;
     },
   },
 });
