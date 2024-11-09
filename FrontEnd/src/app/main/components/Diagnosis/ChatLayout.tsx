0f7e5774cb5f78b5978214fbb6d001e30a19c148
kwonja <kownja87@gmail.com>
2024-11-09 11:05:20 +0900

    feat : sse 에러발생시 5초뒤 재요청 로직 구현

--- FrontEnd/src/app/main/components/Diagnosis/ChatLayout.tsx
+++ FrontEnd/src/app/main/components/Diagnosis/ChatLayout.tsx
@@ -14,7 +14,7 @@ import { useEffect, useState } from 'react';
 import { BiMessageRoundedDots } from 'react-icons/bi';
 import { TbFoldDown, TbFoldUp } from 'react-icons/tb';
 import { CgClose } from 'react-icons/cg';
-import { fetchPatientChat } from '@/apis/patient';
+import { fetchPatientChat } from '@/apis/Patient';
 
 type ChatProps = {
   pid: number;
--- FrontEnd/src/app/main/layout.tsx
+++ FrontEnd/src/app/main/layout.tsx
@@ -1,14 +1,10 @@
-'use client';
 import SEEHandler from '@/components/Alarm/SEEHandler';
-import { useAppSelector } from '@/redux/store/hooks/store';
 import React from 'react';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const { otp } = useAppSelector((state) => state.alarm);
-  console.log(otp);
   return (
     <>
-      {otp !== '' && <SEEHandler otp={otp} />}
+      <SEEHandler />
       {children}
     </>
   );
