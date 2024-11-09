5a8e6fd46cdc9ddb81ae4fd7edad43301d255913
이윤주 <yunj333@gmail.com>
2024-11-09 11:57:00 +0900

    feat: 비밀번호 수정 api 연결 중

--- FrontEnd/src/apis/user.ts
+++ FrontEnd/src/apis/user.ts
@@ -1,18 +1,15 @@
-// import Cookies from 'js-cookie';
 'use server';
 
 import { getCookie } from '@/utils/cookie';
+import { redirect } from 'next/navigation';
 
 export async function updateName(name: string) {
-  console.log('11111111111111111111111111');
   try {
-    // js-cookie를 사용하여 accessToken 가져오기
-    // const accessToken = Cookies.get('accessToken'); // 쿠키 이름이 'accessToken'인 경우
-
     const accessToken = getCookie('accessToken');
 
     if (!accessToken) {
-      throw new Error('토큰이 없습니다.');
+      // 토큰 없으면 로그인 페이지로 리다이렉트
+      redirect('/login?message=login_required');
     }
 
     console.log('엑세스 토큰', accessToken);
@@ -28,7 +25,40 @@ export async function updateName(name: string) {
     });
 
     if (!response.ok) {
-      console.log(response.status);
+      console.log(response);
+      throw new Error('응답이 없습니다.');
+    }
+
+    const data = await response.json();
+    return data.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function updatePW(currentPW: string, password: string) {
+  try {
+    const accessToken = getCookie('accessToken');
+
+    if (!accessToken) {
+      // 토큰 없으면 로그인 페이지로 리다이렉트
+      redirect('/login?message=login_required');
+    }
+
+    console.log('엑세스 토큰', accessToken);
+
+    const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/member/update-password`, {
+      cache: 'no-store',
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({ currentPW: currentPW.trim(), password: password.trim() }),
+    });
+
+    if (!response.ok) {
+      console.log(response);
       throw new Error('응답이 없습니다.');
     }
 
