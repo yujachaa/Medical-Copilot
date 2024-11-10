527ee6acd0c05ba3e83cfb23a6b506f5f6732b14
kwonja <kownja87@gmail.com>
2024-11-10 16:45:52 +0900

    feat : 컴포넌트 지연로딩

--- FrontEnd/src/app/main/components/Diagnosis/components/report/ReportBtn.tsx
+++ FrontEnd/src/app/main/components/Diagnosis/components/report/ReportBtn.tsx
@@ -1,7 +1,8 @@
 'use client';
 
+import dynamic from 'next/dynamic';
 import { useState } from 'react';
-import ExportModal from './ExportModal';
+const ExportModal = dynamic(() => import('./ExportModal'));
 
 export default function ReportBtn() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -11,10 +12,14 @@ export default function ReportBtn() {
   return (
     <>
       <div className="flex-grow flex justify-end items-center gap-3 font-bold">
-        <button className="outline outline-blue-btn text-blue-btn px-2 py-1 rounded-md hover:text-white hover:bg-blue-btn">
+        <button
+          aria-label="save"
+          className="outline outline-blue-btn text-blue-btn px-2 py-1 rounded-md hover:text-white hover:bg-blue-btn"
+        >
           Save
         </button>
         <button
+          aria-label="export report"
           className="outline outline-save text-save hover:bg-save hover:text-white px-2 py-1 rounded-md"
           onClick={openModal}
         >
--- FrontEnd/src/app/main/page.tsx
+++ FrontEnd/src/app/main/page.tsx
@@ -1,12 +1,11 @@
 'use client';
 
 import React from 'react';
-import Main from './components/Default/MainLayout';
 import { useAppSelector } from '@/redux/store/hooks/store';
+import Main from './components/Default/MainLayout';
 import ChatLayout from './components/Diagnosis/ChatLayout';
 import SideBar from '@/components/SideBar/SideBar';
 import TabBoard from '@/components/Tabs/TabBoard/SmallTabBoard';
-// import SideBar from '@/components/SideBar/SideBar';
 
 export default function Page() {
   const { selectedIndex, tablist } = useAppSelector((state) => state.tab);
