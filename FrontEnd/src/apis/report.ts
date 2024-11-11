7253d7b189459340e747b517b2c25f1de81b08d1
이윤주 <yunj333@gmail.com>
2024-11-11 17:51:45 +0900

    feat: 그림 좌표 저장 api 연결

--- FrontEnd/src/apis/report.ts
+++ FrontEnd/src/apis/report.ts
@@ -1,5 +1,9 @@
 import { BaseURL } from './core';
 
+interface CoordinatesGroup {
+  points: { x: number; y: number }[];
+}
+
 export async function fetchReport(reportId: string) {
   try {
     const response = await fetch(`${BaseURL}report/${reportId}`, {
@@ -14,7 +18,45 @@ export async function fetchReport(reportId: string) {
     }
     const data = await response.json();
     return data.data;
-    return;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function updateDrawing(reportId: string, coordinatesGroups: CoordinatesGroup[]) {
+  try {
+    const response = await fetch(`${BaseURL}drawing/${reportId}`, {
+      cache: 'no-store',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ coordinatesGroups: coordinatesGroups }),
+    });
+    if (!response.ok) {
+      throw new Error('응답이 없습니다.');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function fetchDrawing(reportId: string) {
+  try {
+    const response = await fetch(`${BaseURL}drawing/${reportId}`, {
+      cache: 'no-store',
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (!response.ok) {
+      throw new Error('응답이 없습니다.');
+    }
+    const data = await response.json();
+    return data.data;
   } catch (error) {
     console.log(error);
   }
