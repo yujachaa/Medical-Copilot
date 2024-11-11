ee1d651bff721bd0ec39567815b0c46e55941b72
전성모 <jeonsm22@daum.net>
2024-11-11 12:17:18 +0900

    fix: 그래프 누적 현상 해결

--- FrontEnd_Cloud/src/app/main/components/UsageByModule/UsageByModuleGraph.tsx
+++ FrontEnd_Cloud/src/app/main/components/UsageByModule/UsageByModuleGraph.tsx
@@ -18,7 +18,7 @@ export default function UsageByModuleGraph({
             type: 'linear',
             min: 'auto',
             max: 'auto',
-            stacked: true,
+            stacked: false,
             reverse: false,
           }}
           yFormat=" >-.2f"
