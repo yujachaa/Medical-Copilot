5b5ff9e1214ab66ed54f854f4f7c5134a51bc89d
JOON-KR <yyj4147@gmail.com>
2024-11-12 17:06:08 +0900

    fix: kafka serialize

--- BackEnd/onpremise/src/main/java/com/newmes/onpremise/domains/agent/dto/request/AgentRequestDto.java
+++ BackEnd/onpremise/src/main/java/com/newmes/onpremise/domains/agent/dto/request/AgentRequestDto.java
@@ -3,21 +3,23 @@ package com.newmes.onpremise.domains.agent.dto.request;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.newmes.onpremise.domains.patient.domain.Gender;
 import com.newmes.onpremise.domains.patient.domain.Modality;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.time.LocalDate;
-public record AgentRequestDto(
-        String PID,
-        String image,
-        String memberId,
-        LocalDate shootingDate,
-        Gender sex,
-        int age,
-        String comment,
-        String key,
-        Modality agent
-) {
-    public AgentRequestDto withMemberId(String newMemberId) {
-        return new AgentRequestDto(PID, image, newMemberId, shootingDate, sex, age, comment, key, agent);
-    }
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AgentRequestDto{
+        String PID;
+        String image;
+        String memberId;
+        LocalDate shootingDate;
+        Gender sex;
+        int age;
+        String comment;
+        String key;
+        Modality agent;
 }
 
