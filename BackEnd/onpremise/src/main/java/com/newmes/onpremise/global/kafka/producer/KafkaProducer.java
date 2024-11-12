0315ba57975bfa74c6a53d7b4f11c02b3e116340
JOON-KR <yyj4148@naver.com>
2024-11-12 11:11:41 +0900

    fix: kafka debug

--- BackEnd/onpremise/src/main/java/com/newmes/onpremise/global/kafka/producer/KafkaProducer.java
+++ BackEnd/onpremise/src/main/java/com/newmes/onpremise/global/kafka/producer/KafkaProducer.java
@@ -4,6 +4,8 @@ import com.newmes.onpremise.domains.agent.dto.request.AgentRequestDto;
 import com.newmes.onpremise.domains.chat.dto.request.ChatRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -13,7 +15,12 @@ public class KafkaProducer {
     private final KafkaTemplate<String, AgentRequestDto> kafkaTemplate;
 
     public void chatSave(AgentRequestDto agentRequestDto) {
-        kafkaTemplate.send("chat", agentRequestDto);
+        Message<AgentRequestDto> message = MessageBuilder
+                .withPayload(agentRequestDto)
+                .setHeader("spring.json.type.id", AgentRequestDto.class.getName())
+                .build();
+
+        kafkaTemplate.send("chat", message.getPayload());
     }
 
 }
