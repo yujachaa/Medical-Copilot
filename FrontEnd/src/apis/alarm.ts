6e8e51ebfa6a2d3030a889a7850361e884994a77
이윤주 <yunj333@gmail.com>
2024-11-13 22:23:05 +0900

    feat: 알람 리스트 api 연결 완료

--- FrontEnd/src/apis/alarm.ts
+++ FrontEnd/src/apis/alarm.ts
@@ -1,4 +1,8 @@
+'use server';
+
+import { redirect } from 'next/navigation';
 import { BaseURL } from './core';
+import { getCookie } from '@/utils/cookie';
 
 export async function fetchAlarmOTP() {
   try {
@@ -17,4 +21,82 @@ export async function fetchAlarmOTP() {
   } catch (error) {
     console.log(error);
   }
+}
+
+export async function fetchAllAlarm() {
+  try {
+    const accessToken = getCookie('accessToken');
+
+    if (!accessToken) {
+      // 토큰 없으면 로그인 페이지로 리다이렉트
+      redirect('/login?message=login_required');
+    }
+    const response = await fetch(`${BaseURL}notification/all`, {
+      cache: 'no-store',
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    if (!response.ok) {
+      throw new Error('응답이 없습니다.');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function readAlarm(notificationId: number) {
+  try {
+    const accessToken = getCookie('accessToken');
+
+    if (!accessToken) {
+      // 토큰 없으면 로그인 페이지로 리다이렉트
+      redirect('/login?message=login_required');
+    }
+    const response = await fetch(`${BaseURL}notification/${notificationId}`, {
+      cache: 'no-store',
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    if (!response.ok) {
+      throw new Error('응답이 없습니다.');
+    }
+    const data = await response.text();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//(예정) API 만들어지면 수정할 것
+export async function readAllAlarm() {
+  // try {
+  //   const accessToken = getCookie('accessToken');
+  //   if (!accessToken) {
+  //     // 토큰 없으면 로그인 페이지로 리다이렉트
+  //     redirect('/login?message=login_required');
+  //   }
+  //   const response = await fetch(`${BaseURL}notification/`, {
+  //     cache: 'no-store',
+  //     method: 'PATCH',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       Authorization: `Bearer ${accessToken}`,
+  //     },
+  //   });
+  //   if (!response.ok) {
+  //     throw new Error('응답이 없습니다.');
+  //   }
+  //   const data = await response.text();
+  //   return data;
+  // } catch (error) {
+  //   console.log(error);
+  // }
 }
--- FrontEnd/src/components/Alarm/components/Item.tsx
+++ FrontEnd/src/components/Alarm/components/Item.tsx
@@ -2,24 +2,46 @@
 import React from 'react';
 import styles from './Item.module.scss';
 import { CgClose } from 'react-icons/cg';
+import { readAlarm } from '@/apis/alarm';
+
+// 알람 데이터 타입 정의
+type Alarm = {
+  id: number;
+  reportId: string | null;
+  memberId: string;
+  patientId: string;
+  modality: string;
+  createdDate: string;
+};
 
 type ItemProps = {
   className?: string;
+  alarmId: number;
+  alarmData: Alarm;
 };
-export default function Item({ className }: ItemProps) {
-  const handleDelete = () => {
-    console.log('Delete clicked');
+export default function Item({ className, alarmId, alarmData }: ItemProps) {
+  const clickAlarm = async () => {
+    console.log('알람클릭', alarmId, alarmData.reportId);
+    //알람 읽기 api 호출
+    const data = await readAlarm(alarmId);
+    if (data) {
+      console.log('알람읽기 완료', data);
+    }
+    //(예정)클릭한 알람과 연결된 리포트 페이지로 이동하는 부분 넣기!!
   };
   return (
-    <div className={styles.container}>
+    <div
+      className={styles.container}
+      onClick={clickAlarm}
+    >
       <div className="flex flex-col">
         <div className={`${styles.alarm} ${className ? styles[className] : ''}`}>New</div>
-        <div>Here`s notification message for CXR diagnosis arrival about Patient`s Id</div>
+        <div>
+          The {alarmData.modality} diagnosis for Patient ID {alarmData.patientId} has been
+          completed.
+        </div>
       </div>
-      <CgClose
-        className={`${styles.close} cursor-pointer`}
-        onClick={handleDelete}
-      />
+      <CgClose className={`${styles.close}`} />
     </div>
   );
 }
