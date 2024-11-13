edba94345c0c11abe927ea278887b529ae63a0da
이윤주 <yunj333@gmail.com>
2024-11-13 23:35:22 +0900

    feat: 알람 api 연결 완료, patient history 모달 버그 수정, patient history api 연결 중

--- FrontEnd/src/apis/alarm.ts
+++ FrontEnd/src/apis/alarm.ts
@@ -75,28 +75,27 @@ export async function readAlarm(notificationId: number) {
   }
 }
 
-//(예정) API 만들어지면 수정할 것
 export async function readAllAlarm() {
-  // try {
-  //   const accessToken = getCookie('accessToken');
-  //   if (!accessToken) {
-  //     // 토큰 없으면 로그인 페이지로 리다이렉트
-  //     redirect('/login?message=login_required');
-  //   }
-  //   const response = await fetch(`${BaseURL}notification/`, {
-  //     cache: 'no-store',
-  //     method: 'PATCH',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       Authorization: `Bearer ${accessToken}`,
-  //     },
-  //   });
-  //   if (!response.ok) {
-  //     throw new Error('응답이 없습니다.');
-  //   }
-  //   const data = await response.text();
-  //   return data;
-  // } catch (error) {
-  //   console.log(error);
-  // }
+  try {
+    const accessToken = getCookie('accessToken');
+    if (!accessToken) {
+      // 토큰 없으면 로그인 페이지로 리다이렉트
+      redirect('/login?message=login_required');
+    }
+    const response = await fetch(`${BaseURL}notification/readAll`, {
+      cache: 'no-store',
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    if (!response.ok) {
+      throw new Error('응답이 없습니다.');
+    }
+    const data = await response.text();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 }
