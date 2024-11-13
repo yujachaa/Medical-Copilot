ffcda0907aab1cc8282eba35671b59ee78d11f5b
jiyoung-Heo <hgyoung06@gmail.com>
2024-11-13 17:04:11 +0900

    feat: add chat page

--- BackEnd/onpremise/src/main/java/com/newmes/onpremise/domains/chat/controller/ChatController.java
+++ BackEnd/onpremise/src/main/java/com/newmes/onpremise/domains/chat/controller/ChatController.java
@@ -3,8 +3,11 @@ package com.newmes.onpremise.domains.chat.controller;
 import com.newmes.onpremise.domains.chat.dto.response.ChatResponseDto;
 import com.newmes.onpremise.domains.chat.service.ChatService;
 import com.newmes.onpremise.domains.chat.dto.request.ChatRequestDto;
+import com.newmes.onpremise.domains.patient.dto.response.PatientResponseDto;
 import com.newmes.onpremise.global.util.HttpResponseUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,5 +28,18 @@ public class ChatController {
   public ResponseEntity<?> loadAll(@PathVariable("pid") String patientId){
     ChatResponseDto chats = chatService.load(patientId);
     return responseUtil.createResponse(chats);
+  }
+
+  @GetMapping("/recent/{page}/{size}")
+  public ResponseEntity<?> getRecentChats(
+          @PathVariable("page") int page,
+          @PathVariable("size") int size) {
+    try {
+      Page<ChatResponseDto> results = chatService.getRecentChats(page, size);
+      return responseUtil.createResponse(results);
+    } catch (Exception e) {
+      e.printStackTrace();
+      return responseUtil.errorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to retrieve recent patients");
+    }
   }
 }
--- BackEnd/onpremise/src/main/java/com/newmes/onpremise/domains/chat/service/ChatService.java
+++ BackEnd/onpremise/src/main/java/com/newmes/onpremise/domains/chat/service/ChatService.java
@@ -2,11 +2,15 @@ package com.newmes.onpremise.domains.chat.service;
 
 import com.newmes.onpremise.domains.chat.dto.request.ChatRequestDto;
 import com.newmes.onpremise.domains.chat.dto.response.ChatResponseDto;
+import com.newmes.onpremise.domains.patient.dto.response.PatientResponseDto;
+import org.springframework.data.domain.Page;
 
 public interface ChatService {
 
   void add(ChatRequestDto chatDto);
 
   ChatResponseDto load(String patientId);
+
+  Page<ChatResponseDto> getRecentChats(int page, int size);
 
 }
