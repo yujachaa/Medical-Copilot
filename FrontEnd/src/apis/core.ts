4bae2ceadd7962b3065de011f59dce1643c27c3f
전성모 <jeonsm22@daum.net>
2024-11-14 10:12:26 +0900

    fix: url 수정

--- FrontEnd/src/apis/core.ts
+++ FrontEnd/src/apis/core.ts
@@ -1,2 +1,2 @@
 export const BaseURL = 'https://k11s205.p.ssafy.io/onpremise/api/';
-export const GPUURL = 'https://https://k11s205.p.ssafy.io//ai/';
+export const GPUURL = 'https://https://k11s205.p.ssafy.io/ai/';
