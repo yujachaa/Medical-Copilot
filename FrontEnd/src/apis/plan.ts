0323d96cae6eab58f33cfc0eedb78419e265b854
전성모 <jeonsm22@daum.net>
2024-11-14 17:10:40 +0900

    feat: finding impression plan API 연결

---
+++ FrontEnd/src/apis/plan.ts
@@ -0,0 +1,25 @@
+import { GPUURL } from './core';
+import { ReportDataType } from '@/types/report';
+
+export async function fetchPlan(impression: string, reportData: ReportDataType) {
+  try {
+    const response = await fetch(`${GPUURL}generate_plan`, {
+      cache: 'no-store',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        member_id: reportData.memberId,
+        impression: impression,
+      }),
+    });
+    if (!response.ok) {
+      throw new Error('응답이 없습니다.');
+    }
+    const data = await response.json();
+    return data.impression;
+  } catch (error) {
+    console.log(error);
+  }
+}
