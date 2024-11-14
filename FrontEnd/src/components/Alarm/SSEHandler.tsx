c9c6b3c14d46aa0657714b343ce5afc71558fb9f
이윤주 <yunj333@gmail.com>
2024-11-14 14:56:37 +0900

    feat: 알람 팝업 구현

--- FrontEnd/src/components/Alarm/SSEHandler.tsx
+++ FrontEnd/src/components/Alarm/SSEHandler.tsx
@@ -1,8 +1,9 @@
 'use client';
 import { BaseURL } from '@/apis/core';
 import { useAppSelector } from '@/redux/store/hooks/store';
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { jwtDecode } from 'jwt-decode';
+import Popup from './components/Popup';
 
 type Token = {
   email: string;
@@ -15,6 +16,16 @@ type Token = {
 export default function SSEHandler() {
   const { accessToken } = useAppSelector((state) => state.user);
   const eventSourceRef = useRef<EventSource | null>(null);
+  const [isPopupOpen, setIsPopupOpen] = useState<boolean>(true); //팝업 확인용 하드코딩
+  const [isClosing, setIsClosing] = useState<boolean>(false);
+
+  const closePopup = () => {
+    setIsClosing(true);
+    setTimeout(() => {
+      setIsPopupOpen(false);
+      setIsClosing(false);
+    }, 200);
+  };
 
   useEffect(() => {
     const decode: Token = jwtDecode(accessToken);
@@ -38,6 +49,8 @@ export default function SSEHandler() {
         console.log(event);
         console.log(event.data);
         //여기서 알람 팝업 열기
+        setIsPopupOpen(true); // 알람 팝업 열기
+        setIsClosing(false); // 팝업 열릴 때는 닫힘 상태 false
       };
       eventSourceRef.current!.addEventListener('notification', (e: Event) => {
         console.log(e);
@@ -62,5 +75,14 @@ export default function SSEHandler() {
     };
   }, [accessToken]);
 
-  return <></>;
+  return (
+    <>
+      {isPopupOpen && (
+        <Popup
+          onClose={closePopup}
+          isClosing={isClosing}
+        />
+      )}
+    </>
+  );
 }
