ec5798b9074299bee15c19539725c5b9580c7e26
전성모 <jeonsm22@daum.net>
2024-11-15 16:47:38 +0900

    feat: fip 기능 추가

---
+++ FrontEnd/src/apis/fip.ts
@@ -0,0 +1,41 @@
+'use server';
+
+import { BaseURL } from './core';
+
+export async function fetchfip(reportId: string) {
+  try {
+    const response = await fetch(`${BaseURL}onpremise/api/pdf/${reportId}`, {
+      cache: 'no-store',
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (!response.ok) {
+      throw new Error('응답이 없습니다.');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function fetchInitfip(reportId: string) {
+  try {
+    const response = await fetch(`${BaseURL}onpremise/api/pdf/reset/${reportId}`, {
+      cache: 'no-store',
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (!response.ok) {
+      throw new Error('응답이 없습니다.');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
