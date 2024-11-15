0c232cf7248b5c10a3e4ff5ad8195afca80b24d7
전성모 <jeonsm22@daum.net>
2024-11-15 13:35:15 +0900

    feat: dlrjtwjrjt

--- FrontEnd/src/app/medical/temp/[id]/page.tsx
+++ FrontEnd/src/app/medical/temp/[id]/page.tsx
@@ -1,9 +1,12 @@
-'use client';
-export default function Page() {
-  return (
-    <>
-      환자정보를 가져와서 사용해야되는데 페이지마다 선택된 환자가 다를텐데 이것을 어떻게할건가
-      tab마다 선택된 환자 정보를 저장해야할듯? 이부분은 내일하겠습니다..
-    </>
-  );
+import Chat from './ChatLayout';
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+export default function Page({ params }: Props) {
+  console.log(params.id);
+  return <Chat pid={params.id} />;
 }
