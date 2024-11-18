047e185e87afa54c291d198eb090774089708dab
전성모 <jeonsm22@daum.net>
2024-11-18 17:05:13 +0900

    fix: bug

--- FrontEnd_Cloud/src/app/main/components/ClientListBox/ClientListBox.tsx
+++ FrontEnd_Cloud/src/app/main/components/ClientListBox/ClientListBox.tsx
@@ -1,7 +1,6 @@
 import { fetchClientList } from '@/apis/fetchClientList';
 import ClientListItem from '../ClientListItem/ClientListItem';
 import styles from './ClientListBox.module.scss';
-import { IoSearch } from 'react-icons/io5';
 
 type dataType = {
   id: number;
@@ -20,11 +19,10 @@ export default async function ClientListBox() {
     <div className={`${styles.main} p-3 flex flex-col min-h-[600px] gap-3`}>
       <div className={`flex justify-between w-full h-[30px] items-center`}>
         <span className={`${styles.title} text-2xl`}>Client List</span>
-        <IoSearch className={`${styles.search} text-3xl`} />
       </div>
       {data && (
         <div className={`${styles.items} w-full h-full flex flex-col gap-3 overflow-y-scroll`}>
-          {data.map((client) => {
+          {data.reverse().map((client) => {
             return (
               <ClientListItem
                 key={client.id}
