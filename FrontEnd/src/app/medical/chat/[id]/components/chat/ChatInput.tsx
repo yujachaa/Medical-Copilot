e95eb2fa466061e22f52187c20269ea2c3390d4c
이윤주 <yunj333@gmail.com>
2024-11-18 13:10:10 +0900

    fix: 환자챗 무한스크롤 버그 수정, 알람 모두읽기 렌더링 되게 수정

--- FrontEnd/src/app/medical/chat/[id]/components/chat/ChatInput.tsx
+++ FrontEnd/src/app/medical/chat/[id]/components/chat/ChatInput.tsx
@@ -3,38 +3,41 @@
 import styles from './ChatInput.module.scss';
 import { FaDatabase } from '@react-icons/all-files/fa/FaDatabase';
 import Send from '@/assets/images/send.svg';
-import { useState } from 'react';
+import { Dispatch, SetStateAction, useState } from 'react';
 import FilteredPatientDB from './PatientDB/FilteredPatientDB';
 import { fetcMedicalAI, fetchCallAI } from '@/apis/Patient';
 import { useAppDispatch, useAppSelector } from '@/redux/store/hooks/store';
 import {
   setLoading,
   setLoadingTabPathName,
-  setPrevMessageList,
+  // setPrevMessageList,
   tab,
 } from '@/redux/features/tab/tabSlice';
 import { setSelectedTabPathName } from '@/redux/features/request/requestSlice';
+import { MessageType } from '../../ChatLayout';
 
 type Props = {
-  // messagelist: MessageType[];
-  // setMessagelist: Dispatch<SetStateAction<MessageType[]>>;
+  messagelist: MessageType[];
+  setMessagelist: Dispatch<SetStateAction<MessageType[]>>;
   pid: string;
   nowTab: tab;
 };
 
-export default function ChatInput({ nowTab, pid }: Props) {
+export default function ChatInput({ nowTab, pid, messagelist, setMessagelist }: Props) {
   const [isPatientModal, setPatientModal] = useState<boolean>(false);
   const [isNewDb, setIsNewDb] = useState<boolean>(false);
   const [comment, setComment] = useState<string>('');
   const reportData = useAppSelector((state) => state.report.reportData);
   const dispatch = useAppDispatch();
-  const messagelist = nowTab.messageList;
+  // const messagelist = nowTab.messageList;
 
   const CloseModal = () => {
     setPatientModal(false);
   };
 
   const clickSend = () => {
+    if (comment === '') return;
+
     const userMessage = {
       id: '',
       agent: 'MG',
@@ -45,8 +48,8 @@ export default function ChatInput({ nowTab, pid }: Props) {
       reportId: '',
     };
 
-    dispatch(setPrevMessageList([userMessage]));
-    // setMessagelist((prev) => [userMessage, ...prev]);
+    // dispatch(setPrevMessageList([userMessage]));
+    setMessagelist((prev) => [userMessage, ...prev]);
     if (isNewDb) {
       //새 db 선택한경우 -> 에이전트 요청 후 false로 바꾸기
       if (nowTab.patient.modality === 'MG') {
@@ -64,6 +67,8 @@ export default function ChatInput({ nowTab, pid }: Props) {
   };
 
   const handleAgentChat = async () => {
+    console.log('에이전트 요청', nowTab, messagelist);
+
     await fetchCallAI({
       PID: nowTab.patient.pid,
       image: nowTab.patient.image,
@@ -74,32 +79,24 @@ export default function ChatInput({ nowTab, pid }: Props) {
       key: nowTab.patientRequest.key,
       agent: nowTab.patient.modality,
     });
-    const notification =
-      // {
-      //   id: '',
-      //   reportId: '',
-      //   agent: 'CXR',
-      //   comment: `ID: ${nowTab.patient.pid} CXR\n${comment}`,
-      //   question: true,
-      //   createDate: '',
-      //   memberId: '',
-      // },
-      {
-        id: '',
-        reportId: '',
-        agent: 'CXR',
-        comment: `Analyzing the data for ID ${nowTab.patient.pid}.`,
-        question: false,
-        createDate: '',
-        memberId: '',
-      };
+    const notification = {
+      id: '',
+      reportId: '',
+      agent: 'CXR',
+      comment: `Analyzing the data for ID ${nowTab.patient.pid}.`,
+      question: false,
+      createDate: '',
+      memberId: '',
+    };
 
-    dispatch(setPrevMessageList([notification]));
+    // dispatch(setPrevMessageList([notification]));
+    setMessagelist((prev) => [notification, ...prev]);
     dispatch(setLoadingTabPathName(nowTab.pathname));
-    // setMessagelist((prev) => [notification, ...prev]);
+    dispatch(setLoading(true));
   };
 
   const handleMedicalChat = async () => {
+    console.log('메디컬챗 요청', nowTab, messagelist);
     const response = await fetcMedicalAI({
       comment: comment,
       isQuestion: true,
@@ -124,7 +121,8 @@ export default function ChatInput({ nowTab, pid }: Props) {
         createDate: '',
         memberId: '',
       };
-      dispatch(setPrevMessageList([responseMessage]));
+      setMessagelist((prev) => [responseMessage, ...prev]);
+      // dispatch(setPrevMessageList([responseMessage]));
     }
     dispatch(setLoading(false));
   };
@@ -140,7 +138,7 @@ export default function ChatInput({ nowTab, pid }: Props) {
         className={styles.chatInput}
         placeholder="Enter a message"
         onKeyDown={(e) => {
-          if (e.key === 'enter') clickSend();
+          if (e.key === 'Enter') clickSend();
         }}
         onChange={(e) => {
           setComment(e.target.value);
